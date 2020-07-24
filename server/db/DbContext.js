ec05e95dfac2189dfe2b561efe8d1a82e805d137
grantibrees <grantibrees@gmail.com>
2020-07-24 13:18:27 -0600

    Updated DbContext

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,10 +1,8 @@
 import PostSchema from "../models/Post";
-import CommentSchema from "../models/Comment";
 import mongoose from "mongoose";
 
 class DbContext {
   Posts = mongoose.model("Post", PostSchema);
-  Comments = mongoose.model("Comment", CommentSchema);
 }
 
 export const dbContext = new DbContext();
