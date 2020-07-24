2358b8c17efd1a94f2503787e24bdd3b7ba0fcae
grantibrees <grantibrees@gmail.com>
2020-07-24 17:44:38 -0600

    added modal functionality

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -7,7 +7,7 @@ export default class Post {
     this.content = data.content
     this.comments = data.comments
     this.voteCount = data.voteCount || 0
-    this.id = data.id || data._id
+    this._id = data._id
     this.user = data.user || "No User"
   }
 
@@ -20,21 +20,21 @@ export default class Post {
 
         <div class="text-right">${this.user}</div>
 
-        <div class="text-center" data-toggle="modal" data-target="#commentModal" onclick="app.postsController.drawsModal(${this.id})"> ${this.topic} </div>
+        <div class="text-center" data-toggle="modal" data-target="#commentModal" onclick="app.postsController.drawsModal('${this._id}')"> ${this.topic} </div>
 
         <img class="center-block" src="${this.imgUrl}" alt="">
 
-        <div class="text-center" data-toggle="modal" data-target="#commentModal" onclick="app.postsController.drawsModal(${this.id})">${this.comments.length} comments</div>
+        <div class="text-center" data-toggle="modal" data-target="#commentModal" onclick="app.postsController.drawsModal('${this._id}')">${this.comments.length} comments</div>
 
       </div>
 
       <div class="col-3">
 
-        <button onclick="app.votesController.postVote('up', ${this.id})">Up Arrow</button>
+        <button onclick="app.votesController.postVote('up', '${this._id}')">Up Arrow</button>
 
         <p>${this.voteCount}</p>
 
-        <button onclick="app.votesController.postVote('down', ${this.id})">Down Arrow</button>
+        <button onclick="app.votesController.postVote('down', '${this._id}')">Down Arrow</button>
 
       </div>
 
@@ -44,37 +44,27 @@ export default class Post {
   }
 
   get modalTemplate() {
-    let template = /*html*/`
+    let template = ""
+
+    // Post Title, Content, etc.
+    template += /*html*/`
       <div class="row bg-secondary m-5">
-
       <div class="col-9">
-
         <h5 class="text-right">${this.user}</h5>
-
         <h3 class="text-center">${this.topic}</h3>
-
-        <img class="center-block" src="${this.imgUrl}" alt="">
-
+        <img class="center-block" src="${this.imgUrl}" alt=""/>
         <h5 class="text-center">${this.comments.length} comments</h5>
-
       </div>
-      
       <div class="col-3">
-
-        <button onclick="app.votesController.postVote('up', ${this.id})">Up Arrow</button>
-
+        <button onclick="app.votesController.postVote('up', '${this._id}')">Up Arrow</button>
         <p>${this.voteCount}</p>
-
-        <button onclick="app.votesController.postVote('down', ${this.id})">Down Arrow</button>
-
+        <button onclick="app.votesController.postVote('down', '${this._id}')">Down Arrow</button>
       </div>
-
       `
     template += /*html*/`
-      <div class= "card border-dark rounded">
+      <div class="card border-dark rounded">
       <div class="row justify-content-center">
-   
-      <form class="col-12" action="" onsubmit= app.postsController.makeComment(event, ${this.id})>
+      <form class="col-12" action="" onsubmit="app.postsController.makeComment(event, '${this._id}')">
       <div class="form-group">
       <label for="comment"></label>
       <label for="user"></label>
@@ -82,15 +72,20 @@ export default class Post {
         <input type="text" name="comment" class="form-control" placeholder="Add a comment.."></div>
         <input type="text" name="user" class="form-control" placeholder="Add a user.."></div>
         <div class="col-2">
-        <button class='btn btn-success' type = "submit" >Submit</button></div>
+        <button class="btn btn-success" type="submit">Submit</button></div>
         
       </div>
     
       </form>
     </div>
      `
-    this.comments.forEach(comment => template += `
-     <h5>${comment}</h5>
+
+    this.comments.forEach(comment => template += /*html*/`
+      
+      <div class="card border-dark rounded">
+      <div class="row justify-content-center">
+      ${comment.content}
+      </div></div>
      `)
 
     return template
