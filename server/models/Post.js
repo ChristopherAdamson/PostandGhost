e7b3b06c2a8d3c764497e72448494e3450e1a26d
grantibrees <grantibrees@gmail.com>
2020-07-24 12:53:01 -0600

    PostsController complete, comment support added

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,13 +1,22 @@
 import { Schema } from "mongoose";
 const ObjectId = Schema.Types.ObjectId
 
+const commentContent = new Schema(
+  {
+    content: { type: String, required: true },
+    voteCount: { type: Number, required: true },
+    user: { type: String, required: true },
+    postId: { type: ObjectId, ref: "PostId", required: true }
+  });
+
 const Post = new Schema(
   {
     topic: { type: String, required: true },
     imgUrl: { type: String, required: true },
     content: { type: String, required: true },
     voteCount: { type: Number, required: true },
-    user: { type: String, required: true }
+    user: { type: String, required: true },
+    comment: [commentContent]
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
