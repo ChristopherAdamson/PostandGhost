02d8c354728ef0ad52a0e4753a9f93da58f07dbd
tim hopp <timothyjhopp@gmail.com>
2020-07-24 22:38:48 -0600

    MASTER

--- client/app/main.js
+++ client/app/main.js
@@ -1,12 +1,10 @@
 import PostsController from "./Controllers/PostsController.js";
 import VotesController from "./Controllers/VotesController.js"
-import SearchController from "./Controllers/SeachController.js"
 
 class App {
 
   postsController = new PostsController();
   votesController = new VotesController();
-  searchController = new SearchController()
 }
 
 window["app"] = new App();
